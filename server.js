63e735b76e1460256505209c2413bbbb56c1078e
rubenag025 <rubenag025@gmail.com>
2020-09-17 16:51:12 -0500

    adding server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+var EXPRESS = require('express');
+var secure = require('express-force-https'); //Import
+
+const APP = EXPRESS();
+require('dotenv').config({path:'./client/.env'})
+
+
+APP.use(secure) //Set htttps
+
+// Serve static assets if in production 
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    APP.use(EXPRESS.static('client/build')); // Sets static folder
+    APP.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
+const port = process.env.PORT || 5000;
+APP.listen(port, () => console.log(`Server started on port ${port}`));
